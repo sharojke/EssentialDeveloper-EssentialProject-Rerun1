b26614b5fad36d3f96ecae4dceac08b08c88f162
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-09-06 22:41:51 +0300

    Add the `EssentialFeed` framework with a test target

---
+++ EssentialFeed/EssentialFeed/EssentialFeed.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeed.
+FOUNDATION_EXPORT double EssentialFeedVersionNumber;
+
+//! Project version string for EssentialFeed.
+FOUNDATION_EXPORT const unsigned char EssentialFeedVersionString[];
