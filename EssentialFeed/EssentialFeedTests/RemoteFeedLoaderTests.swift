ca156d18ce3df6c861dcf7a0910ec0cf476fd410
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-09-07 17:08:41 +0300

    NM: `RemoteFeedLoader` requests data from URL on load

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -1,17 +1,51 @@
 import XCTest
 
-final class RemoteFeedLoader {
+// swiftlint:disable force_unwrapping
+
+protocol HTTPClient {
+    func get(from url: URL)
 }
 
-final class HTTPClient {
+private final class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    
+    func get(from url: URL) {
+        requestedURL = url
+    }
+}
+    
+final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    func load() {
+        client.get(from: url)
+    }
 }
 
 final class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
-        _ = RemoteFeedLoader()
+        let url = URL(string: "https://a-url.com")!
+        let client = HTTPClientSpy()
+        _ = RemoteFeedLoader(url: url, client: client)
         
         XCTAssertNil(client.requestedURL)
     }
+    
+    func test_load_requestsDataFromURL() {
+        let url = URL(string: "https://a-url.com")!
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURL, url)
+    }
 }
+
+// swiftlint:enable force_unwrapping
