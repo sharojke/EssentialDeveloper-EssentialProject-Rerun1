5a2ae9cd637e545dc6f8798d0947ce29954732f2
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-09-08 15:54:04 +0300

    NM: Move mapping logic to `RemoteFeedItemsMapper`

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -20,38 +20,16 @@ public final class RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success((data, response)):
-                if response.statusCode == 200,
-                   let root = try? JSONDecoder().decode(Root.self, from: data) {
-                    completion(.success(root.items.map { $0.item }))
-                } else {
+                do {
+                    let items = try RemoteFeedItemsMapper.map(data: data, response: response)
+                    completion(.success(items))
+                } catch {
                     completion(.failure(.invalidData))
                 }
                 
             case .failure:
                 completion(.failure(.connectivity))
             }
-        }
-    }
-}
-
-private extension RemoteFeedLoader {
-    struct Root: Decodable {
-        let items: [Item]
-    }
-    
-    struct Item: Decodable {
-        let id: UUID
-        let description: String?
-        let location: String?
-        let image: URL
-        
-        var item: FeedItem {
-            return FeedItem(
-                id: id,
-                description: description,
-                location: location,
-                imageURL: image
-            )
         }
     }
 }
