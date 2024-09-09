41776d5379373883b5cee9d8b40f0b670430ff2b
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-09-09 23:10:50 +0300

    NM: `URLSessionHTTPClient` performs GET request with URL when getting from URL

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -29,9 +29,14 @@ private final class URLProtocolStub: URLProtocol {
     }
     
     private static var stub: Stub?
+    private static var requestObserver: ((URLRequest) -> Void)?
     
     static func stub(data: Data?, response: URLResponse?, error: Error?) {
         stub = Stub(data: data, response: response, error: error)
+    }
+    
+    static func observeRequests(observer: @escaping (URLRequest) -> Void) {
+        requestObserver = observer
     }
     
     static func startInterceptingRequests() {
@@ -41,11 +46,13 @@ private final class URLProtocolStub: URLProtocol {
     static func stopInterceptingRequests() {
         URLProtocol.unregisterClass(Self.self)
         stub = nil
+        requestObserver = nil
     }
     
     // if `true` means we handle this request and it's our responsibility
     // to complete it either with success of failure
     override class func canInit(with request: URLRequest) -> Bool {
+        requestObserver?(request)
         return true
     }
     
@@ -73,6 +80,25 @@ private final class URLProtocolStub: URLProtocol {
 }
 
 final class URLSessionHTTPClientTests: XCTestCase {
+    func test_getFromURL_performsGETRequestWithURL() {
+        URLProtocolStub.startInterceptingRequests()
+        
+        let url = URL(string: "https://a-url.com")!
+        
+        let exp = expectation(description: "Wait for request")
+        URLProtocolStub.observeRequests { request in
+            XCTAssertEqual(request.url, url)
+            XCTAssertEqual(request.httpMethod, "GET")
+            exp.fulfill()
+        }
+        
+        URLSessionHTTPClient().get(from: url) { _ in }
+        
+        wait(for: [exp], timeout: 1)
+        
+        URLProtocolStub.stopInterceptingRequests()
+    }
+    
     func test_getFromURL_failsOnRequestError() {
         URLProtocolStub.startInterceptingRequests()
         
