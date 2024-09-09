c8b8e7e190c32c687df67b7b4831984cc67e9fdc
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-09-09 21:05:21 +0300

    NM: `URLSessionHTTPClient` creates data task with URL on get from URL

---
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -0,0 +1,44 @@
+import XCTest
+
+// swiftlint:disable force_unwrapping
+
+final class URLSessionHTTPClient {
+    private let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func get(from url: URL) {
+        session.dataTask(with: url) { _, _, _ in
+        }
+    }
+}
+
+private final class URLSessionSpy: URLSession {
+    var receivedURLs = [URL]()
+    
+    override func dataTask(
+        with url: URL,
+        completionHandler: @escaping (Data?, URLResponse?, (any Error)?) -> Void
+    ) -> URLSessionDataTask {
+        receivedURLs.append(url)
+        return FakeURLSessionDataTask()
+    }
+}
+
+private final class FakeURLSessionDataTask: URLSessionDataTask {}
+
+final class URLSessionHTTPClientTests: XCTestCase {
+    func test_getFromURL_createsDataTaskWithURL() {
+        let url = URL(string: "https://a-url.com")!
+        let session = URLSessionSpy()
+        let sut = URLSessionHTTPClient(session: session)
+        
+        sut.get(from: url)
+        
+        XCTAssertEqual(session.receivedURLs, [url])
+    }
+}
+
+// swiftlint:enable force_unwrapping
