797e4b51d3c9cad53f730633c8c5f346e2bb90c7
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-09-16 23:33:59 +0300

    PM: Move `FeedStoreSpy` to a shared scope

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -3,43 +3,6 @@ import XCTest
 
 // swiftlint:disable force_unwrapping
 
-private final class FeedStoreSpy: FeedStore {
-    enum Message: Equatable {
-        case deleteCachedFeed
-        case insert([LocalFeedImage], Date)
-    }
-    
-    private(set) var receivedMessages = [Message]()
-    private var deletionCompletions = [(DeleteResult) -> Void]()
-    private var insertionCompletions = [(InsertResult) -> Void]()
-    
-    func deleteCachedFeed(completion: @escaping (DeleteResult) -> Void) {
-        deletionCompletions.append(completion)
-        receivedMessages.append(.deleteCachedFeed)
-    }
-    
-    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping (InsertResult) -> Void) {
-        insertionCompletions.append(completion)
-        receivedMessages.append(.insert(feed, timestamp))
-    }
-    
-    func completeDeletion(with error: Error, at index: Int = .zero) {
-        deletionCompletions[index](.failure(error))
-    }
-    
-    func completeDeletionSuccessfully(at index: Int = .zero) {
-        deletionCompletions[index](.success(Void()))
-    }
-    
-    func completeInsertion(with error: Error, at index: Int = .zero) {
-        insertionCompletions[index](.failure(error))
-    }
-    
-    func completeInsertionSuccessfully(at index: Int = .zero) {
-        insertionCompletions[index](.success(Void()))
-    }
-}
-
 final class CacheFeedUseCaseTests: XCTestCase {
     func test_init_doesNotMessageStoreUponCreation() {
         let (_, store) = makeSUT()
