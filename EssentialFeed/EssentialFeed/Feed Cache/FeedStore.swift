f22aba6c85ab27950dd8daec2e12768fae3ca20f
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-09-21 18:35:55 +0300

    PM: Add documentation to `FeedStore`

--- EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -5,7 +5,15 @@ public protocol FeedStore {
     typealias InsertResult = Result<Void, Error>
     typealias RetrieveResult = Result<LocalFeed, Error>
     
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
     func deleteCachedFeed(completion: @escaping (DeleteResult) -> Void)
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping (InsertResult) -> Void)
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
     func retrieve(completion: @escaping (RetrieveResult) -> Void)
 }
