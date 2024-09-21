d067a3b17fb8c0b8f9e3719258a7e3a5e9d43e65
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-09-21 18:36:12 +0300

    PM: Add documentation to `HTTPClient`

--- EssentialFeed/EssentialFeed/Feed API/HTTPClient.swift
+++ EssentialFeed/EssentialFeed/Feed API/HTTPClient.swift
@@ -3,5 +3,7 @@ import Foundation
 public protocol HTTPClient {
     typealias GetResult = Result<(Data, HTTPURLResponse), Error>
     
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
     func get(from url: URL, completion: @escaping (GetResult) -> Void)
 }
