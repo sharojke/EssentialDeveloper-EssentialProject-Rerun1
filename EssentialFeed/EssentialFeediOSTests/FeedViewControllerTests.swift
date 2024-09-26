27b882ee906111d07dc6b967df7682c9bbd882ef
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-09-26 21:20:38 +0300

    UIM: `FeedViewController` loads feed on view did load

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -1,16 +1,32 @@
 import EssentialFeed
 import XCTest
+import UIKit
 
 final class LoaderSpy {
-    let loadCallCount = 0
+    private(set) var loadCallCount = 0
+    
+    func load() {
+        loadCallCount += 1
+    }
 }
 
 
-final class FeedViewController {
+final class FeedViewController: UIViewController {
     private let loader: LoaderSpy
     
     init(loader: LoaderSpy) {
         self.loader = loader
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loader.load()
     }
 }
 
@@ -20,5 +36,14 @@ final class FeedViewControllerTests: XCTestCase {
         _ = FeedViewController(loader: loader)
         
         XCTAssertEqual(loader.loadCallCount, .zero)
+    }
+    
+    func test_viewDidLoad_loadsFeed() {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(loader.loadCallCount, 1)
     }
 }
