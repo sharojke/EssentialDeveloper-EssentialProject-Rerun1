ff555647be213b36b3d228c1ac33575d7f7a9a86
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-10-03 01:41:57 +0300

    UIM: Replace `FeedImageViewModel` with `FeedImagePresenter`

--- EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -12,13 +12,16 @@ private final class FeedViewAdapter: FeedView {
     
     func display(_ viewModel: FeedViewModel) {
         controller?.tableModel = viewModel.feed.map { feedImage in
-            return FeedImageCellController(
-                viewModel: FeedImageViewModel(
-                    model: feedImage,
-                    imageLoader: loader,
-                    imageTransformer: UIImage.init
-                )
+            let adapter = FeedImageLoadingPresentationAdapter<WeakRefVirtualProxy<FeedImageCellController>, UIImage>(
+                feedImage: feedImage,
+                imageLoader: loader
             )
+            let view = FeedImageCellController(delegate: adapter)
+            adapter.imagePresenter = FeedImagePresenter(
+                view: WeakRefVirtualProxy(view),
+                imageTransformer: UIImage.init
+            )
+            return view
         }
     }
 }
@@ -46,6 +49,40 @@ private final class FeedLoadingPresentationAdapter: FeedRefreshViewControllerDel
     }
 }
 
+private final class FeedImageLoadingPresentationAdapter
+<View: FeedImageLoadingView, Image>: FeedImageCellControllerDelegate
+where View.Image == Image {
+    private let feedImage: FeedImage
+    private let imageLoader: FeedImageDataLoader
+    var imagePresenter: FeedImagePresenter<View, Image>?
+    
+    private var task: FeedImageDataLoaderTask?
+    
+    init(feedImage: FeedImage, imageLoader: FeedImageDataLoader) {
+        self.feedImage = feedImage
+        self.imageLoader = imageLoader
+    }
+    
+    func didRequestImage() {
+        imagePresenter?.didStartLoadingImage(for: feedImage)
+        
+        task = imageLoader.loadImageData(from: feedImage.url) { [weak imagePresenter, feedImage] result in
+            switch result {
+            case .success(let data):
+                imagePresenter?.didFinishLoadingImage(with: data, for: feedImage)
+                
+            case .failure(let error):
+                imagePresenter?.didFinishLoadingImage(with: error, for: feedImage)
+            }
+        }
+    }
+    
+    func didCancelImageRequest() {
+        task?.cancel()
+        task = nil
+    }
+}
+
 private final class WeakRefVirtualProxy<T: AnyObject> {
     private weak var object: T?
     
@@ -62,8 +99,10 @@ public enum FeedUIComposer {
         let presentationAdapter = FeedLoadingPresentationAdapter(feedLoader: feedLoader)
         let refreshController = FeedRefreshViewController(delegate: presentationAdapter)
         let feedController = FeedViewController(refreshController: refreshController)
+        
+        let feedViewAdapter = FeedViewAdapter(controller: feedController, loader: imageLoader)
         let feedPresenter = FeedPresenter(
-            feedView: FeedViewAdapter(controller: feedController, loader: imageLoader),
+            feedView: feedViewAdapter,
             loadingView: WeakRefVirtualProxy(refreshController)
         )
         presentationAdapter.feedPresenter = feedPresenter
@@ -73,6 +112,12 @@ public enum FeedUIComposer {
 
 extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
     func display(_ viewModel: FeedLoadingViewModel) {
+        object?.display(viewModel)
+    }
+}
+
+extension WeakRefVirtualProxy: FeedImageLoadingView where T: FeedImageLoadingView, T.Image == UIImage {
+    func display(_ viewModel: FeedImageLoadingViewModel<UIImage>) {
         object?.display(viewModel)
     }
 }
