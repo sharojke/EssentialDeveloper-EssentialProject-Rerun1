533203d24e3dcfad7be2db599ec457e14b8eaacd
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-10-06 01:10:59 +0300

    UIM: Move `FeedImageLoadingPresentationAdapter` to a separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedImageLoadingPresentationAdapter.swift
@@ -0,0 +1,36 @@
+import EssentialFeed
+import Foundation
+
+final class FeedImageLoadingPresentationAdapter
+<View: FeedImageLoadingView, Image>: FeedImageCellControllerDelegate
+where View.Image == Image {
+    private let feedImage: FeedImage
+    private let imageLoader: FeedImageDataLoader
+    var imagePresenter: FeedImagePresenter<View, Image>?
+    
+    private var task: FeedImageDataLoaderTask?
+    
+    init(feedImage: FeedImage, imageLoader: FeedImageDataLoader) {
+        self.feedImage = feedImage
+        self.imageLoader = imageLoader
+    }
+    
+    func didRequestImage() {
+        imagePresenter?.didStartLoadingImage(for: feedImage)
+        
+        task = imageLoader.loadImageData(from: feedImage.url) { [weak imagePresenter, feedImage] result in
+            switch result {
+            case .success(let data):
+                imagePresenter?.didFinishLoadingImage(with: data, for: feedImage)
+                
+            case .failure(let error):
+                imagePresenter?.didFinishLoadingImage(with: error, for: feedImage)
+            }
+        }
+    }
+    
+    func didCancelImageRequest() {
+        task?.cancel()
+        task = nil
+    }
+}
--- EssentialFeed/EssentialFeediOS/Feed UI/Composers/MainQueueDispatchDecorator.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/MainQueueDispatchDecorator.swift
@@ -1,5 +1,5 @@
-import Foundation
 import EssentialFeed
+import Foundation
 
 final class MainQueueDispatchDecorator<T> {
     private let decoratee: T
