b75ff82908dbdb1c47b094e315ae3d29b8af7ad3
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-10-06 01:10:03 +0300

    UIM: Move `FeedLoadingPresentationAdapter` to a separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedLoadingPresentationAdapter.swift
@@ -0,0 +1,25 @@
+import EssentialFeed
+import Foundation
+
+final class FeedLoadingPresentationAdapter: FeedViewControllerDelegate {
+    private let feedLoader: FeedLoader
+    var feedPresenter: FeedPresenter?
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    func didRequestFeedRefresh() {
+        feedPresenter?.didStartLoadingFeed()
+        
+        feedLoader.load { [weak feedPresenter] result in
+            switch result {
+            case .success(let feed):
+                feedPresenter?.didFinishLoadingFeed(with: feed)
+                
+            case .failure(let error):
+                feedPresenter?.didFinishLoadingWithError(with: error)
+            }
+        }
+    }
+}
--- EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -28,29 +28,6 @@ private final class FeedViewAdapter: FeedView {
     }
 }
 
-private final class FeedLoadingPresentationAdapter: FeedViewControllerDelegate {
-    private let feedLoader: FeedLoader
-    var feedPresenter: FeedPresenter?
-    
-    init(feedLoader: FeedLoader) {
-        self.feedLoader = feedLoader
-    }
-    
-    func didRequestFeedRefresh() {
-        feedPresenter?.didStartLoadingFeed()
-        
-        feedLoader.load { [weak feedPresenter] result in
-            switch result {
-            case .success(let feed):
-                feedPresenter?.didFinishLoadingFeed(with: feed)
-                
-            case .failure(let error):
-                feedPresenter?.didFinishLoadingWithError(with: error)
-            }
-        }
-    }
-}
-
 private final class FeedImageLoadingPresentationAdapter
 <View: FeedImageLoadingView, Image>: FeedImageCellControllerDelegate
 where View.Image == Image {
