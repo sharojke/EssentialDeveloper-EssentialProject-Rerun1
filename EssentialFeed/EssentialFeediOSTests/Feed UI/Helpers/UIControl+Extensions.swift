abf08c6d8a8d202c9f73be3ee533129a84450d8b
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-10-09 20:59:51 +0300

    UIM: Fix a swiftlint warning

--- EssentialFeed/EssentialFeediOSTests/Feed UI/Helpers/UIControl+Extensions.swift
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/Helpers/UIControl+Extensions.swift
@@ -3,8 +3,8 @@ import UIKit
 extension UIControl {
     func simulate(event: UIControl.Event) {
         allTargets.forEach { target in
-            actions(forTarget: target, forControlEvent: event)?.forEach {
-                (target as NSObject).perform(Selector($0))
+            actions(forTarget: target, forControlEvent: event)?.forEach { action in
+                (target as NSObject).perform(Selector(action))
             }
         }
     }
