4b74de1ccc8a442a766c1b18c67e02aebcc76513
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-10-09 21:20:40 +0300

    UIM: `FeedPresenter` does not send messages to view on did start loading feed

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -1,19 +1,43 @@
 import XCTest
 
-final class FeedPresenter {
-    init(view: Any) {
+protocol FeedErrorView {
+    func display(_ viewModel: FeedErrorViewModel)
+}
+
+struct FeedErrorViewModel {
+    let message: String?
+    
+    static func noError() -> Self {
+        return Self(message: nil)
     }
 }
 
-private final class ViewSpy {
-    let messages = [Any]()
+final class FeedPresenter {
+    private let errorView: FeedErrorView
+    
+    init(errorView: FeedErrorView) {
+        self.errorView = errorView
+    }
+    
+    func didStartLoadingFeed() {
+        errorView.display(.noError())
+    }
 }
 
+// swiftlint:disable:next file_types_order
 final class FeedPresenterTests: XCTestCase {
     func test_init_doesNotSendMessagesToView() {
         let (_, view) = makeSUT()
         
         XCTAssertTrue(view.messages.isEmpty)
+    }
+    
+    func test_didStartLoadingFeed_doesNotSendMessagesToView() {
+        let (presenter, view) = makeSUT()
+        
+        presenter.didStartLoadingFeed()
+        
+        XCTAssertEqual(view.messages, [.display(errorMessage: nil)])
     }
     
     // MARK: Helpers
@@ -23,9 +47,23 @@ final class FeedPresenterTests: XCTestCase {
         line: UInt = #line
     ) -> (sut: FeedPresenter, view: ViewSpy) {
         let view = ViewSpy()
-        let presenter = FeedPresenter(view: view)
+        let presenter = FeedPresenter(errorView: view)
         trackForMemoryLeaks(view, file: file, line: line)
         trackForMemoryLeaks(presenter, file: file, line: line)
         return (presenter, view)
+    }
+}
+
+private final class ViewSpy {
+    enum Message: Equatable {
+        case display(errorMessage: String?)
+    }
+    
+    private(set) var messages = [Message]()
+}
+
+extension ViewSpy: FeedErrorView {
+    func display(_ viewModel: FeedErrorViewModel) {
+        messages.append(.display(errorMessage: viewModel.message))
     }
 }
