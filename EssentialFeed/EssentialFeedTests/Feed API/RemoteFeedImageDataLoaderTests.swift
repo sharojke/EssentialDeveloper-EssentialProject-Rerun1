cc27320b93787e2c4cb728d54d9f485958e7e8b8
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-10-10 23:38:25 +0300

    MM: Extract test expectation into a reusable helper method

--- EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -4,7 +4,7 @@ import XCTest
 // swiftlint:disable force_unwrapping
 
 private final class HTTPClientSpy: HTTPClient {
-    private var messages = [(url: URL, completion: (Result<(Data, HTTPURLResponse), Error>) -> Void)]()
+    private var messages = [(url: URL, completion: (GetResult) -> Void)]()
     
     var requestedURLs: [URL] {
         return messages.map { $0.url }
@@ -81,24 +81,10 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
     
     func test_loadImageDataFromURL_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
-        let url = anyURL()
+        let error = anyNSError()
         
-        let exp = expectation(description: "Wait for client completion")
-        var receivedResult: FeedImageDataLoader.LoadImageResult?
-        sut.loadImageData(from: url) { result in
-            receivedResult = result
-            exp.fulfill()
-        }
-        
-        client.complete(with: anyNSError())
-        wait(for: [exp], timeout: 1)
-        
-        switch receivedResult {
-        case .failure:
-            break
-            
-        default:
-            XCTFail("Expected failure, got \(receivedResult as Any) instead")
+        expect(sut, toCompleteWith: .failure(error)) {
+            client.complete(with: error)
         }
     }
     
@@ -113,6 +99,54 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
         trackForMemoryLeaks(client, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, client)
+    }
+    
+    private func expect(
+        _ sut: RemoteFeedImageDataLoader,
+        toCompleteWith expectedResult: FeedImageDataLoader.LoadImageResult,
+        when action: () -> Void,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        let url = anyURL()
+        let exp = expectation(description: "Wait for client completion")
+        
+        sut.loadImageData(from: url) { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedData), .success(expectedData)):
+                XCTAssertEqual(
+                    receivedData,
+                    expectedData,
+                    "Expected \(expectedData), got \(receivedData) instead",
+                    file: file,
+                    line: line
+                )
+                
+            case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+                XCTAssertEqual(
+                    receivedError.domain,
+                    expectedError.domain,
+                    "Expected \(receivedError.domain), got \(receivedError.domain) instead",
+                    file: file,
+                    line: line
+                )
+                XCTAssertEqual(
+                    receivedError.code,
+                    receivedError.code,
+                    "Expected \(receivedError.code), got \(receivedError.code) instead",
+                    file: file,
+                    line: line
+                )
+                
+            default:
+                XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+            }
+            
+            exp.fulfill()
+        }
+        
+        action()
+        wait(for: [exp], timeout: 1)
     }
 }
 
