99cb827960a00d4bb7d69886b9a06b76e0081d77
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-10-15 21:41:58 +0300

    MM: Move `RemoteFeedImageDataLoader` to production

---
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
@@ -0,0 +1,66 @@
+import Foundation
+
+private final class HTTPTaskWrapper: FeedImageDataLoaderTask {
+    private var completion: FeedImageDataLoader.LoadImageResultCompletion?
+    var wrapped: HTTPClientTask?
+    
+    init(completion: @escaping FeedImageDataLoader.LoadImageResultCompletion) {
+        self.completion = completion
+    }
+    
+    func complete(with result: FeedImageDataLoader.LoadImageResult) {
+        completion?(result)
+    }
+    
+    func cancel() {
+        preventFurtherCompletions()
+        wrapped?.cancel()
+        wrapped = nil
+    }
+    
+    private func preventFurtherCompletions() {
+        completion = nil
+    }
+}
+
+public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+    
+    private let client: HTTPClient
+
+    public init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    @discardableResult
+    public func loadImageData(
+        from url: URL,
+        completion: @escaping LoadImageResultCompletion
+    ) -> FeedImageDataLoaderTask {
+        let taskWrapper = HTTPTaskWrapper(completion: completion)
+        let task = client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            
+            let taskResult: FeedImageDataLoader.LoadImageResult
+            defer { taskWrapper.complete(with: taskResult) }
+            
+            switch result {
+            case let .success((data, response)):
+                guard response.statusCode == 200,
+                      !data.isEmpty else {
+                    taskResult = .failure(Error.invalidData)
+                    return
+                }
+                
+                taskResult = .success(data)
+                
+            case .failure(let error):
+                taskResult = .failure(error)
+            }
+        }
+        taskWrapper.wrapped = task
+        return taskWrapper
+    }
+}
