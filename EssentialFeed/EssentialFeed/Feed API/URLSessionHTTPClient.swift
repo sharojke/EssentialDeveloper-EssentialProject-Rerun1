e4ee5b2b6c4479c1622caf1ce05da6a1d85d84b6
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-10-15 20:54:37 +0300

    MM: Remove default `URLSessionHTTPClient` initializer parameter (dependency) as clients should be mindful about which `URLSession` instance to use

--- EssentialFeed/EssentialFeed/Feed API/URLSessionHTTPClient.swift
+++ EssentialFeed/EssentialFeed/Feed API/URLSessionHTTPClient.swift
@@ -13,7 +13,7 @@ private struct URLSessionTaskWrapper: HTTPClientTask {
 public final class URLSessionHTTPClient: HTTPClient {
     private let session: URLSession
     
-    public init(session: URLSession = .shared) {
+    public init(session: URLSession) {
         self.session = session
     }
     
