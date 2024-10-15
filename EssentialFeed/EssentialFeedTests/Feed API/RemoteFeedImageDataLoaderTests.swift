00ba91ade4cfd0199114c84f7582cabbfb290515
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-10-15 21:40:08 +0300

    MM: Extract duplicate `HTTPClientSpy` helper into a shared scope

--- EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -1,51 +1,6 @@
 import EssentialFeed
 import XCTest
 
-// swiftlint:disable force_unwrapping
-
-private final class HTTPClientSpy: HTTPClient {
-    private struct Task: HTTPClientTask {
-        private let onCancel: () -> Void
-        
-        init(onCancel: @escaping () -> Void) {
-            self.onCancel = onCancel
-        }
-        
-        func cancel() {
-            onCancel()
-        }
-    }
-    
-    private var messages = [(url: URL, completion: (GetResult) -> Void)]()
-    private(set) var cancelledURLs = [URL]()
-    
-    var requestedURLs: [URL] {
-        return messages.map { $0.url }
-    }
-    
-    func get(from url: URL, completion: @escaping (GetResult) -> Void) -> HTTPClientTask {
-        messages.append((url, completion))
-        return Task { [weak self] in
-            self?.cancelledURLs.append(url)
-        }
-    }
-    
-    func complete(with error: Error, at index: Int = 0) {
-        messages[index].completion(.failure(error))
-    }
-    
-    func complete(with statusCode: Int, data: Data, at index: Int = 0) {
-        let message = messages[index]
-        let response = HTTPURLResponse(
-            url: message.url,
-            statusCode: statusCode,
-            httpVersion: nil,
-            headerFields: nil
-        )!
-        message.completion(.success((data, response)))
-    }
-}
-
 private final class HTTPTaskWrapper: FeedImageDataLoaderTask {
     private var completion: FeedImageDataLoader.LoadImageResultCompletion?
     var wrapped: HTTPClientTask?
@@ -278,5 +233,3 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
         return .failure(error)
     }
 }
-
-// swiftlint:enable force_unwrapping
