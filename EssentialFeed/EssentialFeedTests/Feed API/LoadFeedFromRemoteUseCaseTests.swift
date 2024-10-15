eb6968c0b7022f895b829eda597366b14ce6d77d
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-10-15 21:40:08 +0300

    MM: Extract duplicate `HTTPClientSpy` helper into a shared scope

--- EssentialFeed/EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
@@ -4,38 +4,6 @@ import XCTest
 // swiftlint:disable force_unwrapping
 // swiftlint:disable force_try
 
-private final class HTTPClientSpy: HTTPClient {
-    private struct Task: HTTPClientTask {
-        func cancel() {}
-    }
-    
-    private var messages = [(url: URL, completion: (Result<(Data, HTTPURLResponse), Error>) -> Void)]()
-    
-    var requestedURLs: [URL] {
-        return messages.map { $0.url }
-    }
-    
-    func get(from url: URL, completion: @escaping (GetResult) -> Void) -> HTTPClientTask {
-        messages.append((url, completion))
-        return Task()
-    }
-    
-    func complete(with error: Error, at index: Int = 0) {
-        messages[index].completion(.failure(error))
-    }
-    
-    func complete(with statusCode: Int, data: Data, at index: Int = 0) {
-        let message = messages[index]
-        let response = HTTPURLResponse(
-            url: message.url,
-            statusCode: statusCode,
-            httpVersion: nil,
-            headerFields: nil
-        )!
-        message.completion(.success((data, response)))
-    }
-}
-
 final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
