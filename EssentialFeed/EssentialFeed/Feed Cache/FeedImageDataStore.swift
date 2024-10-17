4ceee37215c8446c52224cdbcdf2b427d9226d65
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-10-17 22:40:08 +0300

    MM: `LocalFeedImageDataLoader` requests image data insertion for URL on `saveImageDataForURL`

--- EssentialFeed/EssentialFeed/Feed Cache/FeedImageDataStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedImageDataStore.swift
@@ -3,6 +3,10 @@ import Foundation
 public protocol FeedImageDataStore {
     typealias RetrieveResult = Result<Data?, Error>
     typealias RetrieveCompletion = (RetrieveResult) -> Void
+    
+    typealias InsertResult = Result<Void, Error>
+    typealias InsertCompletion = (InsertResult) -> Void
 
     func retrieveData(for url: URL, completion: @escaping RetrieveCompletion)
+    func insert(_ data: Data, for url: URL, completion: @escaping InsertCompletion)
 }
--- EssentialFeed/EssentialFeed/Feed Feature/FeedImageDataLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedImageDataLoader.swift
@@ -8,6 +8,9 @@ public protocol FeedImageDataLoader {
     typealias LoadImageResult = Result<Data, Error>
     typealias LoadImageResultCompletion = (LoadImageResult) -> Void
     
+    typealias SaveImageResult = Result<Void, Error>
+    typealias SaveImageResultCompletion = (SaveImageResult) -> Void
+    
     func loadImageData(
         from url: URL,
         completion: @escaping LoadImageResultCompletion
