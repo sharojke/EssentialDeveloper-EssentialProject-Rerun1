5f50b2f53795cbc2a7fded1cc0fc7e7bb2698f92
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-10-17 22:48:47 +0300

    MM: Extract `FeedImageDataStoreSpy` into a shared scope

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedImageDataStoreSpy.swift
@@ -0,0 +1,35 @@
+import EssentialFeed
+import Foundation
+
+final class FeedImageDataStoreSpy: FeedImageDataStore {
+    enum Message: Equatable {
+        case retrieveData(for: URL)
+        case insert(Data, for: URL)
+    }
+    
+    private(set) var receivedMessages = [Message]()
+    private var retrieveCompletions = [RetrieveCompletion]()
+    private var insertCompletions = [InsertCompletion]()
+    
+    // MARK: Retrieve
+    
+    func retrieveData(for url: URL, completion: @escaping RetrieveCompletion) {
+        receivedMessages.append(.retrieveData(for: url))
+        retrieveCompletions.append(completion)
+    }
+    
+    func completeRetrieval(with error: Error, at index: Int = .zero) {
+        retrieveCompletions[index](.failure(error))
+    }
+    
+    func completeRetrieval(with data: Data?, at index: Int = .zero) {
+        retrieveCompletions[index](.success(data))
+    }
+    
+    // MARK: Insertion
+    
+    func insert(_ data: Data, for url: URL, completion: @escaping InsertCompletion) {
+        receivedMessages.append(.insert(data, for: url))
+        insertCompletions.append(completion)
+    }
+}
