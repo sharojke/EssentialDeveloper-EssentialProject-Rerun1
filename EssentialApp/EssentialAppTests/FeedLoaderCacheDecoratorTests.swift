9ea8a701c522459cf15049490be4ce0645b89649
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-10-19 21:51:37 +0300

    MM: `FeedLoaderCacheDecorator` delivers feed on loader success on `load`

---
+++ EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
@@ -0,0 +1,74 @@
+import EssentialFeed
+import XCTest
+
+final class FeedLoaderCacheDecorator: FeedLoader {
+    private let decoratee: FeedLoader
+    
+    init(decoratee: FeedLoader) {
+        self.decoratee = decoratee
+    }
+    
+    func load(completion: @escaping (LoadResult) -> Void) {
+        decoratee.load(completion: completion)
+    }
+}
+
+private final class FeedLoaderSpy: FeedLoader {
+    private let loadResult: LoadResult
+    
+    init(loadResult: LoadResult) {
+        self.loadResult = loadResult
+    }
+    
+    func load(completion: @escaping (LoadResult) -> Void) {
+        completion(loadResult)
+    }
+}
+
+final class FeedLoaderCacheDecoratorTests: XCTestCase {
+    func test_load_deliversFeedOnLoaderSuccess() {
+        let feed = uniqueFeed()
+        let sut = makeSUT(loadResult: .success(feed))
+        
+        expect(sut, toCompleteWith: .success(feed))
+    }
+    
+    // MARK: Helpers
+    
+    private func makeSUT(
+        loadResult: FeedLoader.LoadResult,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> FeedLoader {
+        let loaderSpy = FeedLoaderSpy(loadResult: loadResult)
+        let sut = FeedLoaderCacheDecorator(decoratee: loaderSpy)
+        trackForMemoryLeaks(loaderSpy, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+    
+    private func expect(
+        _ sut: FeedLoader,
+        toCompleteWith expectedResult: FeedLoader.LoadResult,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        let exp = expectation(description: "Wait for load")
+        sut.load { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedFeed), .success(expectedFeed)):
+                XCTAssertEqual(receivedFeed, expectedFeed, file: file, line: line)
+                
+            case (.failure, .failure):
+                break
+                
+            default:
+                XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1)
+    }
+}
