5f141ca7317a46316e04f61d0cfddfec4fe7bee1
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-10-19 14:22:39 +0300

    MM: Move `CoreDataFeedStore` implementation of the `FeedStore` protocol to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastrucure/CoreData/CoreDataFeedStore+FeedStore.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+extension CoreDataFeedStore: FeedStore {
+    public func insert(
+        _ feed: [LocalFeedImage],
+        timestamp: Date,
+        completion: @escaping FeedStore.InsertCompletion
+    ) {
+        perform { context in
+            do {
+                let managedCache = try ManagedCache.newUniqueInstance(in: context)
+                managedCache.timestamp = timestamp
+                managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
+
+                try context.save()
+                completion(.success(Void()))
+            } catch {
+                context.rollback()
+                completion(.failure(error))
+            }
+        }
+    }
+
+    public func deleteCachedFeed(completion: @escaping FeedStore.DeleteCompletion) {
+        perform { context in
+            do {
+                try ManagedCache.find(in: context)
+                    .map(context.delete)
+                    .map(context.save)
+                completion(.success(Void()))
+            } catch {
+                context.rollback()
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    public func retrieve(completion: @escaping FeedStore.RetrieveCompletion) {
+        perform { context in
+            completion(
+                Result {
+                    try ManagedCache.find(in: context).map { cache in
+                        return CachedFeed(feed: cache.localFeed, timestamp: cache.timestamp)
+                    }
+                }
+            )
+        }
+    }
+}
