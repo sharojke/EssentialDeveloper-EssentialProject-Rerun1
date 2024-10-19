8cee9d1e1614847da437cbfea2900b03812a81a1
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-10-19 18:14:47 +0300

    MM: Add swiftlint to `EssintialApp`

--- EssentialApp/EssentialApp/ViewController.swift
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -1,19 +1,4 @@
-//
-//  ViewController.swift
-//  EssentialApp
-//
-//  Created by Macbook Gray on 10/19/24.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+final class ViewController: UIViewController {
 }
-
