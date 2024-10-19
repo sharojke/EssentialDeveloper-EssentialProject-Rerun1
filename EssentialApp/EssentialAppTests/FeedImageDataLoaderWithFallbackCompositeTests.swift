87c9f2d9925019b44df0b5ec7e4b4de58f0a721a
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-10-19 19:01:28 +0300

    MM: `FeedImageDataLoaderWithFallbackComposite` does not load image on `init`

---
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -0,0 +1,61 @@
+import EssentialFeed
+import XCTest
+
+private final class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
+    private let primary: FeedImageDataLoader
+    private let fallback: FeedImageDataLoader
+    
+    init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {
+        self.primary = primary
+        self.fallback = fallback
+    }
+    
+    func loadImageData(
+        from url: URL,
+        completion: @escaping LoadImageResultCompletion
+    ) -> FeedImageDataLoaderTask {
+        return primary.loadImageData(from: url, completion: completion)
+    }
+}
+
+private final class FeedImageDataLoaderSpy: FeedImageDataLoader {
+    private final class Task: FeedImageDataLoaderTask {
+        func cancel() {}
+    }
+    
+    private(set) var loadedURLs = [URL]()
+    
+    func loadImageData(
+        from url: URL,
+        completion: @escaping LoadImageResultCompletion
+    ) -> FeedImageDataLoaderTask {
+        loadedURLs.append(url)
+        return Task()
+    }
+}
+
+// swiftlint:disable:next type_name
+final class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
+    func test_init_doesNotLoadImage() {
+        let (_, primary, fallback) = makeSUT()
+        
+        XCTAssertTrue(primary.loadedURLs.isEmpty)
+        XCTAssertTrue(fallback.loadedURLs.isEmpty)
+    }
+    
+    // MARK: Helpers
+    
+    private func makeSUT(
+        file: StaticString = #filePath,
+        line: UInt = #line
+        // swiftlint:disable:next large_tuple
+    ) -> (sut: FeedImageDataLoader, primary: FeedImageDataLoaderSpy, fallback: FeedImageDataLoaderSpy) {
+        let primary = FeedImageDataLoaderSpy()
+        let fallback = FeedImageDataLoaderSpy()
+        let sut = FeedImageDataLoaderWithFallbackComposite(primary: primary, fallback: fallback)
+        trackForMemoryLeaks(primary, file: file, line: line)
+        trackForMemoryLeaks(fallback, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, primary, fallback)
+    }
+}
