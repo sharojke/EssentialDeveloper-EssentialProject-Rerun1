7d61d5b2076e8d54601ce04b4a0eb6f74764e956
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-10-19 13:30:51 +0300

    MM: `CoreDataFeedImageDataStore` delivers not found when stored data URL does not match on `retrieveImageData`

--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
@@ -2,12 +2,23 @@ import EssentialFeed
 import XCTest
 
 // swiftlint:disable force_try
+// swiftlint:disable force_unwrapping
 
 final class CoreDataFeedImageDataStoreTests: XCTestCase {
     func test_retrieveImageData_deliversNotFoundWhenEmpty() {
         let sut = makeSUT()
         
         expect(sut, toCompleteRetrievalWith: notFound(), for: anyURL())
+    }
+    
+    func test_retrieveImageData_deliversNotFoundWhenStoredDataURLDoesNotMatch() {
+        let sut = makeSUT()
+        let url = URL(string: "http://a-url.com")!
+        let nonMatchingURL = URL(string: "http://another-url.com")!
+
+        insert(anyData(), for: url, into: sut)
+        
+        expect(sut, toCompleteRetrievalWith: notFound(), for: nonMatchingURL)
     }
     
     // MARK: Helpers
@@ -23,6 +34,10 @@ final class CoreDataFeedImageDataStoreTests: XCTestCase {
         return .success(nil)
     }
     
+    private func localImage(url: URL) -> LocalFeedImage {
+        return LocalFeedImage(id: UUID(), description: "any", location: "any", url: url)
+    }
+    
     private func expect(
         _ sut: CoreDataFeedStore,
         toCompleteRetrievalWith expectedResult: FeedImageDataStore.RetrieveResult,
@@ -33,7 +48,7 @@ final class CoreDataFeedImageDataStoreTests: XCTestCase {
         let exp = expectation(description: "Wait for load completion")
         sut.retrieveData(for: url) { receivedResult in
             switch (receivedResult, expectedResult) {
-            case let (.success( receivedData), .success(expectedData)):
+            case let (.success(receivedData), .success(expectedData)):
                 XCTAssertEqual(receivedData, expectedData, file: file, line: line)
                 
             default:
@@ -43,6 +58,38 @@ final class CoreDataFeedImageDataStoreTests: XCTestCase {
         }
         wait(for: [exp], timeout: 1.0)
     }
+    
+    private func insert(
+        _ data: Data,
+        for url: URL,
+        into sut: CoreDataFeedStore,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        let image = localImage(url: url)
+        let exp = expectation(description: "Wait for insert completion")
+        sut.insert([image], timestamp: Date()) { result in
+            switch result {
+            case .success:
+                sut.insert(data, for: url) { result in
+                    switch result {
+                    case .success:
+                        break
+                        
+                    case .failure(let error):
+                        XCTFail("Failed to insert \(data) with error \(error)", file: file, line: line)
+                    }
+                    exp.fulfill()
+                }
+                
+            case .failure(let error):
+                XCTFail("Failed to save \(image) with error \(error)", file: file, line: line)
+                exp.fulfill()
+            }
+        }
+        
+        wait(for: [exp], timeout: 1)
+    }
 }
 
 extension CoreDataFeedStore: @retroactive FeedImageDataStore {
@@ -51,7 +98,9 @@ extension CoreDataFeedStore: @retroactive FeedImageDataStore {
     }
     
     public func insert(_ data: Data, for url: URL, completion: @escaping InsertCompletion) {
+        completion(.success(Void()))
     }
 }
 
 // swiftlint:enable force_try
+// swiftlint:enable force_unwrapping
