977a3db4fcd52559a147b31ebbabdff770bc98ef
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-10-19 14:22:39 +0300

    MM: Move `CoreDataFeedStore` implementation of the `FeedStore` protocol to separate file

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastrucure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastrucure/CoreData/CoreDataFeedStore.swift
@@ -1,7 +1,7 @@
 import CoreData
 import Foundation
 
-public final class CoreDataFeedStore: FeedStore {
+public final class CoreDataFeedStore {
     public static let modelName = "FeedStore"
     public static let model = NSManagedObjectModel(name: modelName, in: Bundle(for: CoreDataFeedStore.self))
     
@@ -12,59 +12,13 @@ public final class CoreDataFeedStore: FeedStore {
         guard let model = Self.model else {
             throw ModelNotFound(modelName: Self.modelName)
         }
-
+        
         container = try NSPersistentContainer.load(
             name: Self.modelName,
             model: model,
             url: storeURL
         )
         context = container.newBackgroundContext()
-    }
-
-    public func insert(
-        _ feed: [LocalFeedImage],
-        timestamp: Date,
-        completion: @escaping FeedStore.InsertCompletion
-    ) {
-        perform { context in
-            do {
-                let managedCache = try ManagedCache.newUniqueInstance(in: context)
-                managedCache.timestamp = timestamp
-                managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
-
-                try context.save()
-                completion(.success(Void()))
-            } catch {
-                context.rollback()
-                completion(.failure(error))
-            }
-        }
-    }
-
-    public func deleteCachedFeed(completion: @escaping FeedStore.DeleteCompletion) {
-        perform { context in
-            do {
-                try ManagedCache.find(in: context)
-                    .map(context.delete)
-                    .map(context.save)
-                completion(.success(Void()))
-            } catch {
-                context.rollback()
-                completion(.failure(error))
-            }
-        }
-    }
-    
-    public func retrieve(completion: @escaping FeedStore.RetrieveCompletion) {
-        perform { context in
-            completion(
-                Result {
-                    try ManagedCache.find(in: context).map { cache in
-                        return CachedFeed(feed: cache.localFeed, timestamp: cache.timestamp)
-                    }
-                }
-            )
-        }
     }
 
     func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
