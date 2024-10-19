0b53fef6d66936ab128aba7a71c6634aab2cff67
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-10-19 16:14:11 +0300

    MM: Add empty project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Macbook Gray on 10/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
