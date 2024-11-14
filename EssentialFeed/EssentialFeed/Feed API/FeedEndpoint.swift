a43ff74750a80b81439725584d0e55d2fc2cbe74
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-11-14 21:58:45 +0300

    LS5: Set `FeedEndpoint` limit to 10

--- EssentialFeed/EssentialFeed/Feed API/FeedEndpoint.swift
+++ EssentialFeed/EssentialFeed/Feed API/FeedEndpoint.swift
@@ -1,12 +1,23 @@
 import Foundation
 
+// swiftlint:disable force_unwrapping
+
 public enum FeedEndpoint {
     case get
     
     public func url(baseURL: URL) -> URL {
         switch self {
         case .get:
-            return baseURL.appendingPathComponent("/v1/feed")
+            var components = URLComponents()
+            components.scheme = baseURL.scheme
+            components.host = baseURL.host
+            components.path = baseURL.path + "/v1/feed"
+            components.queryItems = [
+                URLQueryItem(name: "limit", value: "10")
+            ]
+            return components.url!
         }
     }
 }
+
+// swiftlint:enable force_unwrapping
