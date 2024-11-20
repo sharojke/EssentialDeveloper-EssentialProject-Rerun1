844d11bbb7e25920dda44f985bde0bbe45869457
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-11-20 22:15:46 +0300

    LS6: Fix for iOS 14.5 prefetching behavior change
    Before we had after running the app:
    cellForRowAt: [0, 0]
    cellForRowAt: [0, 1]
    prefetchRowsAt: [[0, 2]]
    prefetchRowsAt: [[0, 3]]
    prefetchRowsAt: [[0, 4]]
    prefetchRowsAt: [[0, 5]]
    prefetchRowsAt: [[0, 6]]
    prefetchRowsAt: [[0, 7]]
    prefetchRowsAt: [[0, 8]]
    prefetchRowsAt: [[0, 9]]
    
    After we have after running the app:
    cellForRowAt: [0, 0]
    cellForRowAt: [0, 1]
    
    But after a scrolling a bit we have:
    prefetchRowsAt: [[0, 2]] <<<<<<
    prefetchRowsAt: [[0, 3]]
    prefetchRowsAt: [[0, 4]]
    prefetchRowsAt: [[0, 5]]
    prefetchRowsAt: [[0, 6]]
    prefetchRowsAt: [[0, 7]]
    prefetchRowsAt: [[0, 8]]
    prefetchRowsAt: [[0, 9]]
    cellForRowAt: [0, 2] <<<<<<
    
    which causes a race condition

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -49,6 +49,8 @@ extension FeedImageCellController: UITableViewDataSource {
         cell.locationContainer.isHidden = !viewModel.hasLocation
         cell.locationLabel.text = viewModel.location
         cell.descriptionLabel.text = viewModel.description
+        cell.feedImageContainer.isShimmering = true
+        cell.feedImageRetryButton.isHidden = true
         cell.onRetry = { [weak self] in
             self?.delegate.didRequestImage()
         }
