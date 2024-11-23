e2f934c36a04533242dc4a183cc4d1f563f58c7e
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-11-23 17:50:59 +0300

    LS7: Deprecate async APIs in favor of new sync APIs

--- EssentialFeed/EssentialFeed/Feed Cache/FeedImageDataStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedImageDataStore.swift
@@ -6,7 +6,46 @@ public protocol FeedImageDataStore {
     
     typealias InsertResult = Result<Void, Error>
     typealias InsertCompletion = (InsertResult) -> Void
+    
+    func retrieveData(for url: URL) throws -> Data?
+    func insert(_ data: Data, for url: URL) throws
 
+    @available(*, deprecated)
     func retrieveData(for url: URL, completion: @escaping RetrieveCompletion)
+    
+    @available(*, deprecated)
     func insert(_ data: Data, for url: URL, completion: @escaping InsertCompletion)
+}
+
+public extension FeedImageDataStore {
+    func retrieveData(for url: URL) throws -> Data? {
+        let dispatchGroup = DispatchGroup()
+        
+        dispatchGroup.enter()
+        var receivedResult: RetrieveResult?
+        retrieveData(for: url) { result in
+            receivedResult = result
+            dispatchGroup.leave()
+        }
+        
+        dispatchGroup.wait()
+        return try receivedResult?.get()
+    }
+    
+    func insert(_ data: Data, for url: URL) throws {
+        let dispatchGroup = DispatchGroup()
+        
+        dispatchGroup.enter()
+        var receivedResult: InsertResult?
+        insert(data, for: url) { result in
+            receivedResult = result
+            dispatchGroup.leave()
+        }
+        
+        dispatchGroup.wait()
+        try receivedResult?.get()
+    }
+    
+    func retrieveData(for url: URL, completion: @escaping RetrieveCompletion) {}
+    func insert(_ data: Data, for url: URL, completion: @escaping InsertCompletion) {}
 }
